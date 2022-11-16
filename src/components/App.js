2c353b2031be46849cc0e5c526f1df0bbace3af3
Abhishek Raj Gupta <110536209+abhishekraj00@users.noreply.github.com>
2022-11-16 10:21:10 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,42 @@
-import React from 'react'
-import '../styles/App.css';
+import React, { useRef } from "react";
+import "../styles/App.css";
 
 const App = () => {
+  const inupt1Ref = useRef();
+  const inupt2Ref = useRef();
 
-  const handleInput = (event) =>{
-   console.log(event.target.value)
-  }
+  const handleInput = (event) => {
+    if (event.target.id === "text-input") {
+      console.log(inupt1Ref.current.value);
+    } else {
+      console.log(inupt2Ref.current.value);
+    }
+  };
 
   // do not change id of input elements
   return (
     <div id="main">
-      <label htmlFor='text-input'>Text Input:- </label>
-      <input id="text-input" type={'text'} onChange ={(e)=>handleInput(e)} />
+      <label htmlFor="text-input">Text Input:- </label>
+      <input
+        id="text-input"
+        type={"text"}
+        ref={inupt1Ref}
+        onChange={handleInput}
+      />
 
-      <br/>
-      <br/>
+      <br />
+      <br />
 
-      <label htmlFor='num-input'>Number input</label>
-      <input id="num-input"  type={'number'} onChange ={(e)=>handleInput(e)}/>
-      <br/>
+      <label htmlFor="num-input">Number input</label>
+      <input
+        id="num-input"
+        type={"number"}
+        ref={inupt2Ref}
+        onChange={handleInput}
+      />
+      <br />
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
